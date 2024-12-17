d5e93471bfe9d02d143ed17fe5afa8acb20676bd
immanuel <immanuelmruk@gmail.com>
2024-12-17 08:11:51 +0100

    Add cors and example user creation

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
+from flask_cors import CORS
+from models import db, User
+from api.user import user_bp
+from api.mfa import mfa_bp
 from config import Config
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config.from_object(Config)
+db.init_app(app)
+
+CORS(
+    app, resources={
+        r"/api/*": {
+            "origins": Config.CORS_ORIGINS.split(","),
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"],
+            "supports_credentials": True,
+            "max_age": 3600,
+        }
+    },
+)
+
+app.register_blueprint(user_bp)
+app.register_blueprint(mfa_bp)
 
 @app.after_request
 def add_header(response):
@@ -16,6 +37,23 @@ def add_header(response):
 @app.route('/<path:path>')
 def index(path):
     return render_template('index.html')
+
+def create_example_user():
+    user = User.query.filter_by().first()
+    if not user:
+        user = User(username='admin', email='admin@example.com')
+        user.set_password('admin')
+        db.session.add(user)
+        db.session.commit()
+        app.logger.info("Example user created, please change the default credentials!")
+
+def init_db():
+    with app.app_context():
+        db.create_all()
+        create_example_user()
+        app.logger.info("Database initialized")
+
+init_db()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=Config.PORT, debug=True)
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY', '!@?Sup€rS€cretK€y?@!')
     PORT = int(os.getenv('PORT', 5000))
+    CORS_ORIGINS = os.getenv('CORS_ORIGINS', f'http://localhost:{PORT}')
     MAX_LOGIN_ATTEMPTS = int(os.getenv("MAX_LOGIN_ATTEMPTS", 5))
     LOCKOUT_DURATION_MINUTES = int(os.getenv("LOCKOUT_DURATION_MINUTES", 20))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
